8cbc0f82f890137c5331f63bf65265333f997ba3
Alex Ray <alex.ray12@gmail.com>
2019-03-09 13:50:58 +0100

    Set initial configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `Alex Ray Blog`,
+    author: `Alex Ray`,
+    description: `A place to ramble about random things projects.`,
+    siteUrl: `https://alexray.dev/`,
   },
   plugins: [
     {
@@ -63,7 +60,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/profile-pic.png`,
       },
     },
     `gatsby-plugin-offline`,
